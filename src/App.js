2ffd1ccde9f78ce923cb4473f101dbc10bb55f6e
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-04-08 17:13:02 +0700

    Added Home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from "./Component/Navbar/Navbar"
 import "./Style/App.scss"
+import {Switch,Route} from "react-router-dom"
+import home from "./Component/Home/Home"
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Switch>
+        <Route path="/" component={home}/>
+      </Switch>
     </div>
   );
 }
