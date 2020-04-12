ddae34e1e921e05f760fc15ce484cf51333580fb
PUTTIPONG SINTOPPUN <60010742@kmitl.ac.th>
2020-04-13 01:27:47 +0700

    Added Profile Data

--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -6,7 +6,16 @@ export default class Profile extends Component {
         return (
             <div>
                 <Card color="red">
-                    <p className="bigtext whitetext">PROFILE</p>
+                    <p className="contentText whitetext">Nonnapoj Wongvisuthirat [Mirth]</p>
+                    <p className="contentText whitetext">26 Oct 1998</p>
+                    <p/>
+                    <p className="contentText whitetext">Assumption University of Thailand</p>
+                    <p className="contentText whitetext">Communication Art</p>
+                    <p className="contentText whitetext">[2017-Present]</p>
+                    <p/>
+                    <p className="contentText whitetext">Triam Udom Suksa Pattanakarn School</p>
+                    <p className="contentText whitetext">Science and Mathematics Program</p>
+                    <p className="contentText whitetext">[2011-2017]</p>
                 </Card>
                 <Thumb text="PROFILE"/>
             </div>
